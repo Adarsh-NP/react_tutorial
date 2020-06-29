842ee482c05bc4e810fdf0a2dd6e7c38106a8e42
Adarsh-NP <f20180806@goa.bits-pilani.ac.in>
2020-06-29 12:28:27 +0530

    test push
    Signed-off-by: Adarsh-NP <f20180806@goa.bits-pilani.ac.in>

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ class App extends Component {
     return (
     <div className="App">
         <PostForms />
-        {/* <Postlist /> */}
+        <Postlist />
         {/* <ClickCounterTwo />
         <HoverCounterTwo /> */}
         {/* <Counterex
